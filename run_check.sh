f70fe62f42c68b9b45152abbb81bd7ad7f992045
ZhangGuohua <FrankZhang@ZhangGuohuadeMac.local>
2017-05-22 00:41:28 +0800

    V2.0 20170522

--- run_check.sh
+++ run_check.sh
@@ -11,9 +11,14 @@ mkdir -p ./reports_`date +%Y%m%d`
 cat checklist.txt  |grep -v "^#" |grep -v "^$" |awk '{print $1}' | while read ip
 do
 echo $ip
-scp -i $key -o StrictHostKeyChecking=no $CK_HOME/tools/$check_shell $username@$ip:~/
-ssh -tt -n -i $key -o StrictHostKeyChecking=no $username@$ip "./$check_shell"
-scp -i $key -o StrictHostKeyChecking=no $username@$ip:~/healcheck_report_*.tgz ./reports_`date +%Y%m%d`
+ping -c2 -w2 $ip 1>/dev/null 2>&1
+if [ $? -ne 0 ]; then
+      echo "$ip" >> $CK_HOME/reports_`date +%Y%m%d`/unreachable_host.txt
+else
+      scp -i $key -o StrictHostKeyChecking=no $CK_HOME/tools/$check_shell $username@$ip:~/
+      ssh -tt -n -i $key -o StrictHostKeyChecking=no $username@$ip "./$check_shell"
+      scp -i $key -o StrictHostKeyChecking=no $username@$ip:~/healcheck_report_*.tgz ./reports_`date +%Y%m%d`
+fi
 done
 
 cp $CK_HOME/tools/report_head.csv $CK_HOME/reports_`date +%Y%m%d`/report_`date +%Y%m%d`.csv
